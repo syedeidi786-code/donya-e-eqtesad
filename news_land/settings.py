44264459fe3c484021038655f290e359994d159d
syed eidi <syedeidi786@gmail.com>
2026-08-15 17:02:26 +0530

    Update Cloudinary configuration to use environment variables

--- news_land/settings.py
+++ news_land/settings.py
@@ -242,15 +242,15 @@ STORAGES = {
 # ============================================================
 
 CLOUDINARY_STORAGE = {
-    "CLOUD_NAME": os.environ.get("vznkdjoy"),
-    "API_KEY": os.environ.get("589831933446475"),
-    "API_SECRET": os.environ.get("pGUGgiwyeYaF10zZ2Zwbf8tOHxU"),
+    "CLOUD_NAME": os.environ.get("CLOUDINARY_CLOUD_NAME"),
+    "API_KEY": os.environ.get("CLOUDINARY_API_KEY"),
+    "API_SECRET": os.environ.get("CLOUDINARY_API_SECRET"),
 }
 
 cloudinary.config(
-    cloud_name=os.environ.get("vznkdjoy"),
-    api_key=os.environ.get("589831933446475"),
-    api_secret=os.environ.get("pGUGgiwyeYaF10zZ2Zwbf8tOHxU"),
+    cloud_name=os.environ.get("CLOUDINARY_CLOUD_NAME"),
+    api_key=os.environ.get("CLOUDINARY_API_KEY"),
+    api_secret=os.environ.get("CLOUDINARY_API_SECRET"),
 )
 # ============================================================
 # AUTHENTICATION
